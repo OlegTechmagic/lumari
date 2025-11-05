f21b9c857b785588a12d2f94118882521e119ac3
Oleg Naumov <oleh.naumov@techmagic.co>
2025-11-05 11:46:33 +0200

    aws cdk

---
+++ esbuild.config.mjs
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
+build({
+  entryPoints: [],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  sourcemap: false,
+  // target: 'node20',
+  outdir: './lambdas',
+  entryNames: '[name]',
+  target: 'esnext',
+  format: 'cjs',
+  loader: { '.ts': 'ts' },
+  plugins: [esbuildPluginTsc()],
+  //   external: ['mock-aws-s3', '@mapbox'],
+}).catch((_err) => {
+  process.exit(1);
+});
